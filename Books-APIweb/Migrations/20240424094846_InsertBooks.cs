a8d2b1c274f36a0c549b84bde77d1cb3977d4d7d
Jess Nunes <eng.diias@gmail.com>
2024-04-24 07:17:39 -0300

    Migrations of Insert-Books in Database MySQL

---
+++ Books-APIweb/Migrations/20240424094846_InsertBooks.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Books_APIweb.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Books(Isbn,Title,PublishingCompany,Price) " +
+                "Values('9786555606454','Elon Musk','Intrinsica','79.90')");
+
+            mb.Sql("Insert into Books(Isbn,Title,PublishingCompany,Price) " +
+             "Values('9786555646139','Nada Pode Me Ferir','Sextante','59.90')");
+
+            mb.Sql("Insert into Books(Isbn,Title,PublishingCompany,Price) " +
+             "Values('9788542221541','Tiago Brunet','Academia','83.90')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from books");
+        }
+    }
+}
