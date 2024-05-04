78222f07922861d92e13df2051c3e040ec3a250f
Jess Nunes <eng.diias@gmail.com>
2024-05-04 11:37:22 -0300

    Adiction route controllers

--- Books-APIweb/Controllers/BooksController.cs
+++ Books-APIweb/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
 ﻿using Books_APIweb.Context;
+using Books_APIweb.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Books_APIweb.Controllers
 {
@@ -15,6 +17,81 @@ namespace Books_APIweb.Controllers
             _dbContext = dbContext;
         }
 
+        // obter o produto
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            var books = _dbContext.Books;
+
+            if(books is null)
+            {
+                return NotFound();
+            }
+
+            return books;
+        }
+
+        //obter o produto pelo id
+        [HttpGet("{id:int}", Name="GetBook")]
+
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = _dbContext.Books.FirstOrDefault(p=> p.Id == id);
+
+            if(book == null)
+            {
+                return NotFound("Livro não encontrado");
+            }
+            return book;
+        }
+
+        // Atualizar o produto
+        [HttpPost]
+        public ActionResult PostBook(Book book)
+        {
+            if(book == null)
+                return BadRequest();
+            
+            _dbContext.Books.Add(book);
+            _dbContext.SaveChanges();
+
+            return new CreatedAtRouteResult("GetBook", 
+                new { id = book.Id }, book);
+        }
+
+        //inserir produto
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Book book)
+        {
+            if(id!= book.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(book).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+
+            return Ok(book);
+        }
+
+        //Deletar produto
+        [HttpDelete("{id:int}")]
+
+        public ActionResult DeleteBook(int id)
+        {
+            var book = _dbContext.Books.FirstOrDefault(p=> p.Id==id);
+           
+            if(book is null)
+            {
+                return NotFound("Produto não localizado");
+            }
+
+            _dbContext.Books.Remove(book);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+       
 
     }
 }
